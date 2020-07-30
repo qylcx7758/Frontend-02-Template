084957ba75780eba70f88f3dc30607f671d54161
“e <qiuyiliang@armvm.com>
2020-07-30 10:11:57 +0800

    week 4

--- week04/toy-browser/client.js
+++ week04/toy-browser/client.js
@@ -217,17 +217,17 @@ class TrunkedBodyParser{
 }
 
 
-void async function(){
+ !async function(){
     let request = new Request({
         method: "POST",
         host: "127.0.0.1",
-        port: "8088",
+        port: "8099",
         path: "/",
         headers:{
             ["X-Foo2"]:"customed"
         },
-        body: {
-            name:"suky"
+        body:{
+            name:"max chiu"
         }
     })
 
